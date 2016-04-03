44fea7e46607e8c4ed8331a327e408677cbfec55
cketti <cketti@gmail.com>
2016-04-03 04:44:58 +0200

    Add JavaDoc comments

--- SafeContentResolver-v14/src/main/java/de/cketti/safecontentresolver/SafeContentResolverCompat.java
+++ SafeContentResolver-v14/src/main/java/de/cketti/safecontentresolver/SafeContentResolverCompat.java
@@ -22,8 +22,25 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 
 
+/**
+ * Helper for creating a {@code SafeContentResolver} instance.
+ *
+ * <p>
+ * The functionality to retrieve the User ID that owns a file was added to the framework in API 21. On versions prior
+ * to that this library uses a small native code module to ask the operating system for the UID.
+ * </p><p>
+ * This helper takes care of instantiating the appropriate implementation for the Android version the app is running
+ * on.
+ * </p>
+ */
 public final class SafeContentResolverCompat {
 
+    /**
+     * Create a {@code SafeContentResolver} instance appropriate for the Android version the app is running on.
+     *
+     * @param context
+     *         {@link Context} used to retrieve a {@link ContentResolver} instance.
+     */
     @NonNull
     public static SafeContentResolver newInstance(@NonNull Context context) {
         //noinspection ConstantConditions
--- SafeContentResolver/src/main/java/de/cketti/safecontentresolver/SafeContentResolver.java
+++ SafeContentResolver/src/main/java/de/cketti/safecontentresolver/SafeContentResolver.java
@@ -25,6 +25,7 @@ import java.io.InputStream;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
@@ -32,10 +33,43 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 
+/**
+ * A replacement for {@code ContentResolver} that protects against the <em>Surreptitious Sharing</em> attack when
+ * opening {@code file://} URIs.
+ *
+ * <p>
+ * When performing a Surreptitious Sharing attack a malicious app tries to trick the user into sending a file private
+ * to the target app to an attacker.
+ * </p><p>
+ * Example:<br>
+ * An {@link Intent#ACTION_SEND} intent that contains the attacker's email address as recipient and a {@code file://}
+ * URI pointing to the file the email client uses to store account passwords. The attacking app itself doesn't have
+ * access to this file because Android's security model prevents that. However, the email client has no trouble
+ * opening a file that belongs to itself. If the user believes this to be a legitimate email with a legitimate
+ * attachment, e.g. because the malicious app asked them to send a bug report, they will send the email and thereby
+ * unknowingly expose their email account passwords to the attacker.
+ * </p><p>
+ * If your app is a receiver of such {@code Intents} and you want to protect against this attack, replace all
+ * occurrences of {@link ContentResolver#openInputStream(Uri)} with {@link SafeContentResolver#openInputStream(Uri)}
+ * from this class. {@code SafeContentResolver} will refuse to open {@code file://} URIs pointing to files belonging to
+ * this app.
+ * </p><p>
+ * On older Android versions apps have the ability to create <a href="https://en.wikipedia.org/wiki/Hard_link">hard
+ * links</a> to files they don't have read or write access to. That means we can't simply check the absolute file path
+ * to learn if a file is stored in our own app-private directory. Instead, we use the {@code fstat} system call to
+ * retrieve the User ID the file belongs to. And if it's identical to the UID of this process, access is denied.
+ * </p>
+ */
 public abstract class SafeContentResolver {
     private final ContentResolver contentResolver;
 
 
+    /**
+     * Create a {@link SafeContentResolver} instance.
+     *
+     * @param context
+     *         {@link Context} used to retrieve a {@link ContentResolver} instance.
+     */
     @NonNull
     public static SafeContentResolver newInstance(@NonNull Context context) {
         //noinspection ConstantConditions
@@ -51,6 +85,22 @@ public abstract class SafeContentResolver {
         this.contentResolver = contentResolver;
     }
 
+    /**
+     * Open a stream to the content associated with a URI.
+     *
+     * <p>
+     * If the provided URI is not a {@code file://} URI, {@link ContentResolver#openInputStream(Uri)} is used to open a
+     * stream. If it is a {@code file://}, this method makes sure the file isn't owned by this app.
+     * </p>
+     *
+     * @param uri
+     *         The URI pointing to the content to access.
+     *
+     * @return {@code InputStream} to access the content.
+     *
+     * @throws FileNotFoundException
+     *         If the provided URI could not be opened or if it points to a file owned by this app.
+     */
     @Nullable
     public InputStream openInputStream(@NonNull Uri uri) throws FileNotFoundException {
         //noinspection ConstantConditions
